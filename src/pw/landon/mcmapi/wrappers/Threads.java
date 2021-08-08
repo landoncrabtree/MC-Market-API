b4032e31916eba67c5ed50165517b4bcbae4117a
Landon Crabtree <me@landon.pw>
2021-08-08 13:24:53 -0500

    Changed Thread wrapper method names.

--- src/pw/landon/mcmapi/wrappers/Threads.java
+++ src/pw/landon/mcmapi/wrappers/Threads.java
@@ -18,22 +18,22 @@ public class Threads {
 
     public static final Gson gson = new Gson();
 
-    public static String listSelfThreads() {
+    public static String list() {
         Token token = Wrapper.token;
         return HTTPUtils.sendGETRequest("https://api.mc-market.org/v1/threads", token);
     }
 
-    public static String getThreadInformation(int thread_id) {
+    public static String getThread(int thread_id) {
         Token token = Wrapper.token;
         return HTTPUtils.sendGETRequest("https://api.mc-market.org/v1/threads/" + thread_id, token);
     }
 
-    public static String listThreadReplies(int thread_id) {
+    public static String listReplies(int thread_id) {
         Token token = Wrapper.token;
         return HTTPUtils.sendGETRequest("https://api.mc-market.org/v1/threads/" + thread_id + "/replies", token);
     }
 
-    public static void replyToThread(int thread_id, String message) {
+    public static void reply(int thread_id, String message) {
         Token token = Wrapper.token;
         Map<String, Object> data = new HashMap<>();
         data.put("message", message);
